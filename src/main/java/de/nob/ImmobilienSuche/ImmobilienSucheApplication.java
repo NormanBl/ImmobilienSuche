43388c4b3e0602542f09049faf730dc97d3c7f68
Norman Blaeker <norman.blaeker@gmail.com>
2024-06-11 22:35:49 +0200

    initial commit

---
+++ src/main/java/de/nob/ImmobilienSuche/ImmobilienSucheApplication.java
@@ -0,0 +1,13 @@
+package de.nob.ImmobilienSuche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmobilienSucheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmobilienSucheApplication.class, args);
+	}
+
+}
